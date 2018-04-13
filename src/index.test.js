2a05c605512e7ab378526ae24b7764ccb871e57e
Wei-Wen Chang <wchang@xghealth.com>
2018-04-13 13:26:22 -0400

    test

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,21 @@
 import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
-/* eslint-disable no-constant-condition*/
-describe('Our first first test', () => {
-    if ('should pass', () => {
-        expect(true).to.equal(false);
-    });
 
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
 });
 
 describe('index.html', () => {
-    it('should say hello', (done) => {
-        const index = fs.readFileSync('./src/index.html', "utf-8");
-        jsdom.env(index, function (err, window) {
-            const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hi");
-            done();
-            window.close();
-        });
+  it('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("User");
+      done();
+      window.close();
     });
-});
+  })
+})
